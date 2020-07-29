d18471c2314cdd1f2a0d948c1dce8b0c0002c576
Aitor Trujillo <lonesniper11@gmail.com>
2020-07-29 15:57:22 +0200

    add userinput

--- app/src/App.js
+++ app/src/App.js
@@ -5,29 +5,41 @@ import { NavBar } from './components/NavBar';
 import { Profile } from './components/Profile';
 import { ReposContainer } from './components/ReposContainer';
 import { retrieveUser, retrieveRepos } from 'logic';
+import { UserInput } from './components/UserInput';
 
 function App() {
+  const [view, setView] = useState('landing')
   const [user, setUser] = useState({})
   const [repos, setRepos] = useState([])
 
-  useEffect(() => {
-    (async () => {
-      const _user = await retrieveUser()
-      setUser(_user)
-      const _repos = await retrieveRepos()
-      setRepos(_repos)
-    })()
-  }, [])
+  // useEffect(() => {
+  //   (async () => {
+  //     const _user = await retrieveUser()
+  //     setUser(_user)
+  //     const _repos = await retrieveRepos()
+  //     setRepos(_repos)
+  //     setView('landing')
+  //   })()
+  // }, [])
 
-  return (
-    <div className="App">
+  const handleOnSubmit = async (query) => {
+    const _user = await retrieveUser()
+    setUser(_user)
+    const _repos = await retrieveRepos()
+    setRepos(_repos)
+    setView('landing')
+  }
+
+  return (<>
+    {view === 'landing' && <UserInput onSubmit={handleOnSubmit} />}
+    {view === 'home' && <div className="App">
       <NavBar reposCount={user.public_repos} />
       <section className='main' >
         {user && <Profile user={user} />}
         {repos && <ReposContainer repos={repos} />}
       </section>
-    </div>
-  );
+    </div>}
+  </>);
 }
 
 export default App;
--- app/src/components/UserInput.js
+++ app/src/components/UserInput.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Search } from './Search'
 
-export const UserInput = () => {
+
+export const UserInput = ({ onSubmit }) => {
+    const [query, setQuery] = useState('')
+
+    const handleOnChange = (e) => {
+        const { value } = e.target
+        console.log(value)
+        setQuery(value)
+    }
+
     return (
-        <div>
-            <Search />
-        </div>
-    )
+        <section className="user-input">
+            <div className="user-input__container">
+                <img className="user-input__logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.flaticon.com%2Ficons%2Fpng%2F512%2F25%2F25231.png&f=1&nofb=1" alt="" />
+                <div className="user-input__title">Find a GitHub Profile</div>
+                <input
+                    onChange={handleOnChange}
+                    className="search__input search__input--large"
+                    type="text" name="search"
+                    placeholder="Find a github profile... (ex: Aitor1111)"
+                />
+                <button className="button button--huge" onSubmit={() => onSubmit(query)}>Find</button>
+            </div>
+        </section>
+    );
 }
